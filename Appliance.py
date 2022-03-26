87550f64b1c79d0b6083a45055c474d6ac10c701
Rusco <russfrancis99@gmail.com>
2022-03-26 13:00:01 +0000

    v.1.0 Appliance Project 1 file Appliance Py Contains class definitions - Energy Supplier Class - Appliance (Abstract Base Class including calculate cost method) - Subclasses of Appliance ABC include: Toaster Class, Vacuum Class, Fridge Class
    Signed-off-by: Rusco <russfrancis99@gmail.com>

--- Appliance.py
+++ Appliance.py
@@ -1,26 +1,26 @@
-
 class EnergySupplier(object):
     """Energy_Supplier class defines the attributes and methods of an energy supplier"""
 
     supplier_dict = {
-                     "EDF"     : {"standard": 0.17, "basic":0.16 ,"Green":0.18},
-                     "PowerGen": {"standard": 0.16, "basic": 0.16,"Green":0.17}
+        "EDF": {"standard": 0.17, "basic": 0.16, "Green": 0.18},
+        "PowerGen": {"standard": 0.16, "basic": 0.16, "Green": 0.17}
     }
 
     default_supplier = "EDF"
     default_plan = "basic"
     default_tariff = supplier_dict[default_supplier][default_plan]
 
-    def __init__(self, name = default_supplier, plan=default_plan, tariff=default_tariff):
+    def __init__(self, name=default_supplier, plan=default_plan, tariff=default_tariff):
         self.name = name
         self.plan = plan
         self.tariff = tariff
 
     def __str__(self):
-        return ("{} is an energy supplier that charges £ {} on plan {}.".format(self.name, self.tariff, self.plan))
+        return "{} is an energy supplier that charges £ {} on plan {}.".format(self.name, self.tariff, self.plan)
 
     def get_tariff(self):
         return self.tariff
+
 
 class Appliance(object):
     """The Appliance class is  a base class for household appliances"""
@@ -51,7 +51,9 @@ class Appliance(object):
         total_kw = self.power_consumption * hours + minutes * rate_per_minute + seconds * rate_per_second
         total_cost_pence = total_kw * self.energy_supplier.tariff
         total_cost_pounds = round(total_cost_pence / 100, 2)
-        return_string = f"Your {self.name} rated a {self.power_consumption} kwh costs a total of £ {total_cost_pounds} to run for {hours} hours, {minutes} minutes and {seconds} seconds"
+        return_string = f"Your {self.name} rated a {self.power_consumption} kwh costs a total of " \
+                        f"£ {total_cost_pounds}" \
+                        f" to run for {hours} hours, {minutes} minutes and {seconds} seconds "
         return return_string
 
 
@@ -88,6 +90,7 @@ class Fridge(Appliance):
         self.function = "Keeps food items cool"
         self.fridge_type = fridge_type
         self.set_power_consumption(Fridge.default_power_consumption)
+
 
 class Oven(Appliance):
     """ Fridge class is a subclass of Appliance and defines the Attributes and methods of a refrigerator"""
